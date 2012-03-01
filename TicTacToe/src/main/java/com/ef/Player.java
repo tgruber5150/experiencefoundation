efe68042a5ecad065cd1f663cdf45f803b18b8f1
BJ Allmon <bjallmon@gmail.com>
2012-02-29 23:00:13 -0500

    changed git project structure for including multiple projects under experiencefoundation.

---
+++ TicTacToe/src/main/java/com/ef/Player.java
@@ -0,0 +1,17 @@
+package com.ef;
+
+
+public class Player {
+
+    private PlayerType playerType;
+
+
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+
+    public void setPlayerType(PlayerType playerType) {
+        this.playerType = playerType;
+    }
+
+}
